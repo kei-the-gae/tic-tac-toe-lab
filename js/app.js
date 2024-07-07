df70fa780f3b72fb512bdef94053d8f2d833128f
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-07 15:22:18 -0700

    Define game state variables

--- js/app.js
+++ js/app.js
@@ -1,20 +1,19 @@
-/*-------------------------------- Constants --------------------------------*/
+//1) Define the required variables used to track the state of the game.
 
+let board = "";
+let turn = "";
+let winner = "";
+let tie = "";
 
+//2) Store cached element references.
 
-/*---------------------------- Variables (state) ----------------------------*/
+//3) Upon loading, the game state should be initialized, and a function should 
+//   be called to render this game state.
 
+//4) The state of the game should be rendered to the user.
 
+//5) Define the required constants.
 
-/*------------------------ Cached Element References ------------------------*/
+//6) Handle a player clicking a square with a `handleClick` function.
 
-
-
-/*-------------------------------- Functions --------------------------------*/
-
-
-
-/*----------------------------- Event Listeners -----------------------------*/
-
-
-
+//7) Create Reset functionality.
